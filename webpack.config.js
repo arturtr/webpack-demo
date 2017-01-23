e6dead565bbf7f24426b124340b78c47b7cc92f8
Artur Trofimov <arturtr@gmail.com>
2017-01-23 21:16:39 +1000

    add HMR for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack= require('webpack')
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -28,8 +29,56 @@ const common = {
   ]
 }
 
+const developmentConfig = {
+  devServer: {
+    // Enable history API fallback so HTML5 History API based
+    // routing works. This is a good default that will come
+    // in handy in more complicated setups.
+    historyApiFallback: true,
+
+    // Unlike the cli flag, this doesn't set
+    // HotModuleReplacementPlugin!
+    hot: true,
+
+    // Don't refresh if hot loading fails. If you want
+    // refresh behavior, set inline: true instead.
+    // hotOnly: true,
+    inline: true,
+
+    // Display only errors to reduce the amount of output.
+    stats: 'errors-only',
+
+    // Parse host and port from env to allow customization.
+    //
+    // If you use Vagrant or Cloud9, set
+    // host: options.host || '0.0.0.0';
+    //
+    // 0.0.0.0 is available to all network devices
+    // unlike default `localhost`.
+    host: process.env.HOST, // Defaults to `localhost`
+    port: process.env.PORT, // Defaults to 8080
+  },
+  plugins: [
+    // Enable multi-pass compilation for enhanced performance
+    // in larger projects. Good default.
+    new webpack.HotModuleReplacementPlugin({
+      // Disabled as this won't work with html-webpack-template yet
+      //multiStep: true,
+    }),
+  ],
+}
+
 module.exports = function(env) {
-  console.log('env', env)
-  
-  return common
+  if (env === 'production') {
+    return common
+  }
+
+  return Object.assign(
+    {},
+    common,
+    developmentConfig,
+    {
+      plugins: common.plugins.concat(developmentConfig.plugins)
+    }
+  )
 }
